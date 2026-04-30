0ba15691fc7ccc86b703bb9844ec5679435d53d4
1987seanmyers-create <1987seanmyers@gmail.com>
2026-04-30 02:53:13 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,8 +5,3 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "StackMinds AI is running"}
-app/static/index.html
-app/static/style.css
-app/static/app.js
-app/static/manifest.webmanifest
-app/static/service-worker.js
